8a0de004a80180974e1977bf42233fea94a5a31e
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-22 17:03:14 -0300

    feat: implementando o método search com fetch

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-export const search = () => {};
+"user strict";
+export const search = () => fetch("https://api.spotify.com/v1/search");
 export const searchAlbums = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
