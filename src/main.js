615cf936fd736b9649e3dd447505db3b4d9e2cd5
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-23 14:26:48 -0300

    feat: criando os métodos searchArtists, searchAlbums, searchTracks e searchPlaylist

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 'user strict';
 export const search = (query, type) =>
 	fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then((data) => data.json());
-export const searchAlbums = () => {};
-export const searchArtists = () => {};
-export const searchTracks = () => {};
-export const searchPlaylist = () => {};
+
+export const searchArtists = (query) => search(query, 'artist');
+
+export const searchAlbums = (query) => search(query, 'album');
+
+export const searchTracks = (query) => search(query, 'track');
+
+export const searchPlaylist = (query) => search(query, 'playlist');
