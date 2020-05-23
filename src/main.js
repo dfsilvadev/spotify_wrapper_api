f01d30105b54a2e962efdb3b3f63f5763ada71fa
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-23 15:59:08 -0300

    feat: modificando o método fetch de search para autenticar com o token spotify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 'user strict';
-export const search = (query, type) =>
-	fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then((data) => data.json());
+export const search = (query, type) => {
+	const accessToken =
+		'BQB7A8_eGO8t0kpqMGhhYeffUWR72PaUgx3iCu3DdoRMa7IY_6lnQlkjhJ0gTR36NbeAXeN8r8gGfqFBdsqeDtaNp5YRF1yVVSGZn0_EkHQsXWf_viU1KGFFLsn5s_HIWqcURCWvxN6dOwI';
+
+	return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${accessToken}`
+		}
+	}).then((data) => data.json());
+};
 
 export const searchArtists = (query) => search(query, 'artist');
 
