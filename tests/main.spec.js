94bfbd919968b88ca4112cc2ed18f2573d96e99e
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-23 13:27:48 -0300

    feat:testando a chamada de fetch com os argumentos certos

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -41,6 +41,21 @@ describe("Spotify Wrapper", () => {
             const artists = search();
 
             expect(fetchedStub).to.have.been.calledOnce;
+            fetchedStub.restore();
+        });
+
+        it("should receive the correct url to fetch", () => {
+            const fetchedStub = sinon.stub(global, "fetch");
+
+            const artists = search("Incubus", "artist");
+            expect(fetchedStub).to.have.been.calledWith(
+                "https://api.spotify.com/v1/search?q=Incubus&type=artist"
+            );
+
+            const albums = search("Incubus", "album");
+            expect(fetchedStub).to.have.been.calledWith(
+                "https://api.spotify.com/v1/search?q=Incubus&type=album"
+            );
         });
     });
 });
