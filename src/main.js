fdfed136ce9d19f494da0f9b4b3cd747b76face1
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-23 13:57:06 -0300

    feat: tratando o retorno da promise em search com then

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-"user strict";
+'user strict';
 export const search = (query, type) =>
-    fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`);
+	fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then((data) => data.json());
 export const searchAlbums = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
