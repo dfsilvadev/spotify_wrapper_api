c40d8af7bdc02e0f99daead45ac1a847d47426e4
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-24 18:35:22 -0300

    feat: criando a index.js para exportar os endpoints de search e getAlbum

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { search, searchArtists, searchAlbums, earchTracks, searchPlaylist } from './search';
+import { getAlbum, getAlbums, getAlbumTracks } from './album';
+
+module.exports = {
+	search,
+	searchArtists,
+	searchAlbums,
+	earchTracks,
+	searchPlaylist,
+	getAlbum,
+	getAlbums,
+	getAlbumTracks
+};
