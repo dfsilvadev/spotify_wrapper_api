b0b6454ded0273a3d5e282678a22c443920cf963
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-24 18:29:42 -0300

    feat: refatorando o código de album.js

--- src/album.js
+++ src/album.js
@@ -1,16 +1,15 @@
 'user strict';
-export const getAlbum = (id) => {
-	const accessToken =
-		'BQADZEGzgTiXv0My1CUkoZGSUCfQp0nnRNb-f7bZFrzqs3nzlgD89H7XKFaXWb4kVrIWBHH9sa6N_9U--aBlxnFjnJ0aSmCYcc_-AdoTKeK6_GYgMvG8mnmtCfIP4MJJWAIfhs_I5u3Ya_Q';
+import { obj, API_URL } from '../configuration/config';
+import { toJSON } from './utils';
 
-	return fetch(`https://api.spotify.com/v1/albums/${id}`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`
-		}
-	}).then((data) => data.json());
+export const getAlbum = (id) => {
+	return fetch(`${API_URL}/albums/${id}`, obj).then(toJSON);
 };
 
-export const getAlbumTracks = () => {};
+export const getAlbums = (ids) => {
+	return fetch(`${API_URL}/albums?ids=${ids}`, obj).then(toJSON);
+};
+
+export const getAlbumTracks = (id) => {
+	return fetch(`${API_URL}/albums/${id}/tracks`, obj).then(toJSON);
+};
