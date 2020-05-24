a4cefc762702ed770fbc57f8c53cf555f96e8f3b
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-24 15:11:50 -0300

    feat: transferindo as responsabilidades de autenticação para token.js

---
+++ accessToken/token.js
@@ -0,0 +1,12 @@
+'user strict';
+const accessToken =
+	'BQD1nQ4XNg1sDrjEKGb9chF8gGMML_2KDfCs37ZemTbwHZNq8crPQ-UTwJJ8oi7DoNK8hj3CtfGBEMXeKj9vkaTZWNUh6bnZYHuy3jMA-Hp8e_KHJR5hJO_ktjWe7clzfqHnMX4Sa8wvpt8';
+
+export const obj = {
+	method: 'GET',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${accessToken}`
+	}
+};
