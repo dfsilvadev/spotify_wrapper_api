55546ebc66a47e4193a46698df7b4d25dd9916b4
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-24 18:32:07 -0300

    feat: criando os testes para os métodos getAlbums e getAlbumTracks

--- tests/albums.spec.js
+++ tests/albums.spec.js
@@ -1,5 +1,5 @@
 import chai, { expect } from 'chai';
-import { getAlbum, getAlbumTracks } from '../src/album';
+import { getAlbum, getAlbums, getAlbumTracks } from '../src/album';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
@@ -55,6 +55,68 @@ describe('Album', () => {
 			promise.resolves({ album: 'name' });
 			const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
 			expect(album.resolveValue).to.be.eql({ album: 'name' });
+		});
+	});
+
+	describe('getAlbums', () => {
+		/**
+         * verifica se o fetch ocorre
+         * verifica se o fetch ocorre com a URL correta
+         * verifica se o dado é recebido pela promise
+        */
+
+		it('should call fetch method', () => {
+			const albums = getAlbums();
+			expect(stubedFetch).to.have.been.calledOnce;
+		});
+
+		it('should call fetch with the correct URL', () => {
+			const albums = getAlbums([ '4aawyAB9vmqN3uQ7FjRGT', '382ObEPsp2rxGrnsizN5TX' ]);
+			expect(stubedFetch).to.have.been.calledWith(
+				'https://api.spotify.com/v1/albums?ids=4aawyAB9vmqN3uQ7FjRGT,382ObEPsp2rxGrnsizN5TX'
+			);
+
+			const albumsTwo = getAlbums([ '4aawyAB9vmqN3uQ7FjRGT', '382ObEPsp2rxGrnsizN5TX' ]);
+			expect(stubedFetch).to.have.been.calledWith(
+				'https://api.spotify.com/v1/albums?ids=4aawyAB9vmqN3uQ7FjRGT,382ObEPsp2rxGrnsizN5TX'
+			);
+		});
+
+		it('should return the correct Data from Promise', () => {
+			promise.resolves({ album: 'name' });
+			const albums = getAlbums([ '4aawyAB9vmqN3uQ7FjRGT', '382ObEPsp2rxGrnsizN5TX' ]);
+			expect(albums.resolveValue).to.be.eql({ album: 'name' });
+		});
+	});
+
+	describe('getAlbumTracks', () => {
+		/**
+         * verifica se o fetch ocorre
+         * verifica se o fetch ocorre com a URL correta
+         * verifica se o dado é recebido pela promise
+        */
+
+		it('should call fetch method', () => {
+			const albumTracks = getAlbumTracks();
+			expect(stubedFetch).to.have.been.calledOnce;
+		});
+
+		it('should call fetch with the correct URL', () => {
+			const albumTracks = getAlbumTracks('4aawyAB9vmqN3uQ7FjRGT');
+			expect(stubedFetch).to.have.been.calledWith(
+				'https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGT/tracks'
+			);
+
+			const albumTracksTwo = getAlbumTracks('4aawyAB9vmqN3uQ7FjRGT');
+			expect(stubedFetch).to.have.been.calledWith(
+				'https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGT/tracks'
+			);
+		});
+
+		it('should return the correct Data from Promise', () => {
+			promise.resolves({ album: 'name' });
+			const albumTracks = getAlbumTracks('4aawyAB9vmqN3uQ7FjRGT');
+			expect(albumTracks.resolveValue).to.be.eql({ album: 'name' });
 		});
 	});
 });
