ea039e8dccfc560aa3e9a418d406a56c76846c99
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-24 15:13:20 -0300

    feat: removendo as responsabilidades de autenticação do método search

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 'user strict';
-export const search = (query, type) => {
-	const accessToken =
-		'BQADZEGzgTiXv0My1CUkoZGSUCfQp0nnRNb-f7bZFrzqs3nzlgD89H7XKFaXWb4kVrIWBHH9sa6N_9U--aBlxnFjnJ0aSmCYcc_-AdoTKeK6_GYgMvG8mnmtCfIP4MJJWAIfhs_I5u3Ya_Q';
+import { obj } from '../accessToken/token';
 
-	return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`
-		}
-	}).then((data) => data.json());
+export const search = (query, type) => {
+	return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, obj).then((data) => data.json());
 };
 
 export const searchArtists = (query) => search(query, 'artist');
