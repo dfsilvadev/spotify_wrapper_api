b4f69fbef0856d25df4e93fc87614ece90d068d2
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-25 16:10:56 -0300

    feat: refatorando para Classe/Construtor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import { search, searchArtists, searchAlbums, earchTracks, searchPlaylist } from './search';
-import { getAlbum, getAlbums, getAlbumTracks } from './album';
+import {
+    search,
+    searchArtists,
+    searchAlbums,
+    earchTracks,
+    searchPlaylist
+} from "./search";
+import { getAlbum, getAlbums, getAlbumTracks } from "./album";
+import { API_URL } from "../configuration/config";
 
-module.exports = {
-	search,
-	searchArtists,
-	searchAlbums,
-	earchTracks,
-	searchPlaylist,
-	getAlbum,
-	getAlbums,
-	getAlbumTracks
-};
+// module.exports = {
+// 	search,
+// 	searchArtists,
+// 	searchAlbums,
+// 	earchTracks,
+// 	searchPlaylist,
+// 	getAlbum,
+// 	getAlbums,
+// 	getAlbumTracks
+// };
+
+export default class SpotifyWrapper {
+    constructor(options) {
+        this.apiURL = options.apiURL || API_URL;
+        this.token = options.token;
+    }
+}
