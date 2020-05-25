cbe701c21bf1acf71ba3207d0ece0872e12d89d7
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-25 16:34:49 -0300

    feat: criando os testes para o método fetch

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,5 +1,13 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
 import SpotifyWrapper from "../src/index";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import sinonStubPromise from "sinon-stub-promise";
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require("node-fetch");
 
 describe("Spotify Wrapper Library", () => {
     it("should create an instace of SpotifyWrapper", () => {
@@ -25,5 +33,59 @@ describe("Spotify Wrapper Library", () => {
             token: "foo"
         });
         expect(spotify.token).to.be.equal("foo");
+    });
+
+    describe("request method", () => {
+        let stubedFetch;
+        let promise;
+
+        beforeEach(() => {
+            stubedFetch = sinon.stub(global, "fetch");
+            promise = stubedFetch.returnsPromise({ album: "name" });
+        });
+
+        afterEach(() => {
+            stubedFetch.restore();
+        });
+
+        it("should have request method", () => {
+            let spotify = new SpotifyWrapper({});
+            expect(spotify.request).to.exist;
+        });
+
+        it("should call fetch when request", () => {
+            let spotify = new SpotifyWrapper({
+                token: "foo"
+            });
+            spotify.request("url");
+
+            expect(stubedFetch).to.have.been.calledOnce;
+        });
+
+        it("should call fetch with right url passed", () => {
+            let spotify = new SpotifyWrapper({
+                token: "foo"
+            });
+            spotify.request("url");
+
+            expect(stubedFetch).to.have.been.calledWith("url");
+        });
+
+        it("should call fetch with right headers passed", () => {
+            let spotify = new SpotifyWrapper({
+                token: "foo"
+            });
+            spotify.request("url");
+            const headers = {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer foo`
+                }
+            };
+
+            expect(stubedFetch).to.have.been.calledWith("url", headers);
+        });
     });
 });
