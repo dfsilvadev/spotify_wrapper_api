a0b5704f0bf611c937aaf0bd6bd1170356a65075
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-26 20:21:12 -0300

    feat: atualizando token pra testes

--- example/albums.js
+++ example/albums.js
@@ -1,11 +1,11 @@
-"use strict";
-import SpotfyWrapper from "../src/index";
-global.fetch = require("node-fetch");
+'use strict';
+import SpotfyWrapper from '../src/index';
+global.fetch = require('node-fetch');
 
 const spotify = new SpotfyWrapper({
-    token: `BQB6OoUYZO7z6_hS8zXXTF3pTrqvHy0I8Z8OMy0CKZzC4NYIPcmITvDOKrePSKyKFO2rlLEWgHBUfh-L84eXt_g5ekMnHBs5MsiDAX5Al9DzDutuK1HF3sORcyR8OGPy5vICmOdkWdesjxc`
+	token: `BQBXd2gITZHyl8wCDFbfp090q_KlqjNgJX27ZveJ77U4b_nD8IKddKnGXIYaIr80VnX0ZBRtW-zNXCLAH2q7qwuPFgA7Nr6MaI_FAdnvQsHzCiXbXp_PhWjFXASigxIqNg97YbC3pLkQcWk`
 });
-const albums = spotify.search.albums("Henrique e Juliano");
-albums.then(data => {
-    return data.albums.items.map(item => console.log(item.name));
+const albums = spotify.search.albums('Aldair Playboy');
+albums.then((data) => {
+	return data.albums.items.map((item) => console.log(item.name));
 });
