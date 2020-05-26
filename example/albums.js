2fa0a75a0df7b5673ddb03398e3db48afbc49c7b
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-26 16:05:39 -0300

    feat: atualizando os exemplos e testando no node e navegador

--- example/albums.js
+++ example/albums.js
@@ -1,5 +1,11 @@
+"use strict";
+import SpotfyWrapper from "../src/index";
 global.fetch = require("node-fetch");
-import { searchAlbums } from "../src/search.js";
 
-const albums = searchAlbums("Henrique e Juliano");
-albums.then(data => data.albums.items.map(item => console.log(item.name)));
+const spotify = new SpotfyWrapper({
+    token: `BQB6OoUYZO7z6_hS8zXXTF3pTrqvHy0I8Z8OMy0CKZzC4NYIPcmITvDOKrePSKyKFO2rlLEWgHBUfh-L84eXt_g5ekMnHBs5MsiDAX5Al9DzDutuK1HF3sORcyR8OGPy5vICmOdkWdesjxc`
+});
+const albums = spotify.search.albums("Henrique e Juliano");
+albums.then(data => {
+    return data.albums.items.map(item => console.log(item.name));
+});
