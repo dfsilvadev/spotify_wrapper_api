2447ffb121840714eba9fdaf302754cd5345af72
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-26 16:01:41 -0300

    feat: atualizando webpack para gerar UMD a partir das classes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,26 +1,25 @@
+import { join } from "path";
 
-import {join} from 'path'
-
-const include = join(__dirname, 'src')
+const include = join(__dirname, "src");
 
 export default {
-  entry: './src/index.js',
-  output: {
-    path: join(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: 'SpotifyWrapper',
-  },
-  devtool: 'source-map',
-  module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: [ [ 'es2015', { modules: false } ] ]
-				}
-			}
-		]
-	}
-}
+    entry: "./index.js",
+    output: {
+        path: join(__dirname, "dist"),
+        libraryTarget: "umd",
+        library: "SpotifyWrapper"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: [["es2015", { modules: false }]]
+                }
+            }
+        ]
+    }
+};
