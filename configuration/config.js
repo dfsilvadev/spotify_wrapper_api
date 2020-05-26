23abac4e450b6a45619d0b5f6f1178ef00098417
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-26 20:21:55 -0300

    feat: deletando pasta configuration

--- configuration/config.js
+++
@@ -1,5 +0,0 @@
-"user strict";
-const TOKEN_API =
-    "BQBgk8gGe69oXKu596qsiBdyvbZtHA7niuaVxz2eeLjr88DCvQE3fDZefEIT4qDz0vjbrK9Gd-NmGb4oD9lNx41O--Q9KrBahH1tnyEZ33X9jCV1lRBrTg61TfY5tufWUsgHtwCMqW6nWbU";
-
-export const API_URL = "https://api.spotify.com/v1";
